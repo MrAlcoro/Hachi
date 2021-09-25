0e1ef3aadca049327f4c3d6ac676acb0f9adfe37
MrAlcoro <alcoro13@gmail.com>
2021-09-25 14:37:58 +0200

    Deleting SDL_mixer and dlls

--- Hachi/Application.cpp
+++ Hachi/Application.cpp
@@ -5,7 +5,6 @@ Application::Application()
 {
 	window = new ModuleWindow(this);
 	input = new ModuleInput(this);
-	audio = new ModuleAudio(this, true);
 	scene_intro = new ModuleSceneIntro(this);
 	renderer3D = new ModuleRenderer3D(this);
 	camera = new ModuleCamera3D(this);
@@ -13,7 +12,6 @@ Application::Application()
 	AddModule(window);
 	AddModule(camera);
 	AddModule(input);
-	AddModule(audio);
 	AddModule(scene_intro);
 	AddModule(renderer3D);
 }
--- Hachi/Application.h
+++ Hachi/Application.h
@@ -6,7 +6,6 @@
 #include "Module.h"
 #include "ModuleWindow.h"
 #include "ModuleInput.h"
-#include "ModuleAudio.h"
 #include "ModuleSceneIntro.h"
 #include "ModuleRenderer3D.h"
 #include "ModuleCamera3D.h"
@@ -21,7 +20,6 @@ public:
 
 	ModuleWindow* window;
 	ModuleInput* input;
-	ModuleAudio* audio;
 	ModuleSceneIntro* scene_intro;
 	ModuleRenderer3D* renderer3D;
 	ModuleCamera3D* camera;
